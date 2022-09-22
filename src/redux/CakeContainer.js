ffa0b6452526b6aaa0a9999f73c0538814acda0e
akashsoni0786 <akashsoni@cedcoss.com>
2022-09-22 19:27:23 +0530

    Done

--- src/redux/CakeContainer.js
+++ src/redux/CakeContainer.js
@@ -10,8 +10,19 @@ const CakeContainer = (props) => {
   }
   return (
     <div>
-      <h2>Total number of cakes: {initialState.numOfcakes}</h2>
-      <h2>Number of cakes : {props.numOfcakes}</h2>
+      <p><b>1.</b> Create a redux react app and initialise a initial state for numberOfCakes = 10</p>
+      <h2>Initial number of cakes: {initialState.numOfcakes}</h2>
+      <hr />
+      <p><b>2.</b> Create a reducer that update the initial state numberOfCakes on every BUY_CAKE action.
+    <br />
+    <b>3.</b> Make sure user can't buy cake from store if numberOfCake = 0
+    <br />
+    <b>4.</b>
+    Create a action as BUY_CAKE to buy a cake from store.
+    <br />
+    <b>5.</b>On buy cake button click, print the updated value of numberOfCakes
+      </p>
+      <h2>Updating number of cakes : {props.numOfcakes}</h2>
       {props.numOfcakes === 0 ? (
         <>
           <h2 style={{color:"red"}}>Can't buy stock is over!</h2>
@@ -19,7 +30,8 @@ const CakeContainer = (props) => {
       ) : (
         <><button onClick={props.buycake}>Buy Cake</button></>
       )}
-      
+      <hr />
+      <p><b>6.</b>Print the initial state of cake store (always)</p>
       <button onClick={props.resetcake}>Reset</button>
     </div>
   );
